f96b0043daa5231e7e67256172f53e36d1e3e66f
tkppr79 <as.takashi.tanimoto@gmail.com>
2020-11-09 13:05:52 +0000

    Add navbar & footer components

---
+++ frontend/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-footer class="pa-2" app>
+    <v-row justify="center" no-gutters>
+      <v-col class="text-center mt-0">
+        <p class="my-2">© 2020 Myzaico.</p>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
---
+++ frontend/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app-bar flat clipped-left app>
+    <div class="d-flex mx-auto" :style="{ width: '60%' }">
+      <v-toolbar-title>Myzaico</v-toolbar-title>
+
+      <v-spacer />
+
+      <v-btn text @click="$router.push('/')">アイテム一覧</v-btn>
+      <v-btn text @click="$router.push('/itemcreate')">アイテム登録</v-btn>
+      <v-btn text @click="$router.push('/settings')">設定</v-btn>
+      <v-btn outlined >ログイン</v-btn>
+    </div>
+  </v-app-bar>
+</template>

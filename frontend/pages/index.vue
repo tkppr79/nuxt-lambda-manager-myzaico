223611e4980cb3225c4ca13b031377a5f0e740b7
tkppr79 <as.takashi.tanimoto@gmail.com>
2020-11-10 13:42:41 +0000

    Add item list page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,93 +1,119 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
+  <v-row justify="center" align="start" no-gutters>
+    <v-col cols="12" lg="3">
+      <v-card outlined class="sidebar mr-3">
+        <h2 class="text-center">フィルター</h2>
+
+        <div>
+          <v-card-title class="justify-center">OR条件</v-card-title>
+        </div>
+
+        <div>
+          <v-card-title class="justify-center">AND条件</v-card-title>
+        </div>
+      </v-card>
+    </v-col>
+
+    <v-col cols="12" lg="9">
+      <v-card outlined class="item-table">
+        <v-col cols="12" lg="9" class="mx-auto">
+          <v-select
+            :items="viewResults"
+            :value="itemsPerPage"
+            @input="itemsPerPage = parseInt($event, 10)"
+            label="表示件数"
+            dense
+            outlined
+            class="ml-4 my-3"
+            :style="{ width: '11%' }"
+          />
+
+          <v-data-table
+            :headers="headers"
+            :items="items"
+            :page.sync="page"
+            :items-per-page="itemsPerPage"
+            hide-default-footer
+            @page-count="pageCount = $event"
+            class="elevation-1 px-4"
           >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
+            <template v-slot:item.name="{ item }">
+              <router-link :to="`/items/${item.id}`" >
+                {{ item.name }}
+              </router-link>
+            </template>
+
+            <template v-slot:item.actions="{ item }">
+              <v-btn
+                elevation="0"
+                color="primary"
+                small
+              >
+                編集
+              </v-btn>
+              <v-btn
+                elevation="0"
+                color="primary"
+                small
+              >
+                削除
+              </v-btn>
+            </template>
+          </v-data-table>
+
+          <v-pagination v-model="page" :length="pageCount" class="pt-2" />
+        </v-col>
       </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Vue from 'vue';
 
-export default {
-  components: {
-    Logo,
-    VuetifyLogo,
-  },
-}
+export default Vue.extend({
+  data: () => ({
+    items: [
+      { id: 1, tags: 'アパレル', name: 'testItem_1', sku: 'AAAAA', manufacturer: 'メーカーA', releaseDate: '2020-10-01', listPrice: 3000 },
+      { id: 2, tags: '家電', name: 'testItem_2', sku: 'BBBBB', manufacturer: 'メーカーB', releaseDate: '2020-10-05', listPrice: 2000 },
+      { id: 3, tags: 'パソコン', name: 'testItem_3', sku: 'CCCCC', manufacturer: 'メーカーC', releaseDate: '2020-10-10', listPrice: 3000 },
+      { id: 4, tags: '本', name: 'testItem_4', sku: 'DDAAAD', manufacturer: 'メーカーD', releaseDate: '2020-10-15', listPrice: 4000 },
+      { id: 5, tags: 'ゲーム', name: 'testItem_5', sku: 'EEEEE', manufacturer: 'メーカーE', releaseDate: '2020-10-20', listPrice: 1000 },
+      { id: 6, tags: '食品', name: 'testItem_6', sku: 'FFFFF', manufacturer: 'メーカーF', releaseDate: '2020-10-25', listPrice: 7000 },
+    ],
+    page: 1,
+    pageCount: 0,
+    itemsPerPage: 5,
+    viewResults: [5, 10, 20, 30, 40, 50, 100],
+    headers: [
+      { text: 'タグ', value: 'tags', sortable: false },
+      { text: 'アイテム名', value: 'name', align: 'start' },
+      { text: 'SKU', value: 'sku' },
+      { text: '製造元', value: 'manufacturer' },
+      { text: '発売日', value: 'releaseDate' },
+      { text: '定価', value: 'listPrice' },
+      { text: 'アクション', value: 'actions', sortable: false },
+    ],
+  }),
+});
 </script>
+
+<style lang="scss">
+
+.sidebar {
+  h2 {
+    background-color: lightgray;
+    color: white;
+  }
+}
+
+.item-table {
+  min-height: 750px;
+
+  .v-text-field__details,
+  .v-messages {
+    min-height: 0;
+  }
+}
+
+</style>
